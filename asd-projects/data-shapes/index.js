576cf2723499e72a55c6106496d794257eb91fe7
aleyahmitchell-tech <aleyah.mitchell@inspire35.org>
2026-03-05 16:17:24 +0000

    message

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -56,13 +56,23 @@ $(document).ready(function () {
   }
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic(data) {
+    setBackgroundWithObject(data);
+    animalDetails.displayType = 1;
+  }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat);
+    animalDetails.displayType = 2;
+  }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+  function handleBad (data, repeat) {
+    repeat = repeat + 1;
+    setBackgroundWithMixed(data, repeat);
+    animationDetails.displayType = 3;
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -70,17 +80,21 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+  handleStatic(dataShapes[currentIndex]);
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+  var currentShape = dataShapes[currentIndex];
+  handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape.repeat;
+
+    handleBad(currentShape, repeat);
   }
 
   /////////////////////////////////////////////////
